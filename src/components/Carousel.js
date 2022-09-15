b5853c76b4e16b064a131b8c3ce22c0d031f9f8d
SatrioBayu <sbayu875@gmail.com>
2022-09-15 15:00:08 +0700

    feat: edit carousel;

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -3,22 +3,20 @@ import styles from "../assets/css/Carousel.module.css";
 const Carousel = () => {
   return (
     <div id="carouselExampleControls" class="carousel carousel-dark slide" data-bs-ride="carousel">
-      <div class={`carousel-inner ${styles["img-height"]}`}>
-        <div class="carousel-item active">
+      <div class={`carousel-inner`}>
+        <div class={`carousel-item active ${styles["img-height"]}`}>
+          <a href="/ajukanSurat">
+            <img src="https://img.freepik.com/free-vector/online-document-concept-illustration_114360-5453.jpg?w=2000" class="d-block w-100" alt="..." />
+          </a>
+        </div>
+        <div class={`carousel-item ${styles["img-height"]}`}>
           <img
             src="https://img.freepik.com/premium-vector/loudspeaker-megaphone-bullhorn-yellow-background-empty-banner-template-promotion-design_578506-256.jpg?w=2000"
             class="d-block w-100"
             alt="..."
           />
         </div>
-        <div class="carousel-item">
-          <img
-            src="https://img.freepik.com/premium-vector/loudspeaker-megaphone-bullhorn-yellow-background-empty-banner-template-promotion-design_578506-256.jpg?w=2000"
-            class="d-block w-100"
-            alt="..."
-          />
-        </div>
-        <div class="carousel-item">
+        <div class={`carousel-item ${styles["img-height"]}`}>
           <img
             src="https://img.freepik.com/premium-vector/loudspeaker-megaphone-bullhorn-yellow-background-empty-banner-template-promotion-design_578506-256.jpg?w=2000"
             class="d-block w-100"
